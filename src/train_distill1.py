d94061069227f13e0f9e3a7da245cd2743b61ece
Kenta Takatsu <kt426@cornell.edu>
2018-12-03 22:36:44 -0500

    get_prob input issue

--- src/train_distill1.py
+++ src/train_distill1.py
@@ -183,7 +183,7 @@ def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None, m
                 probs = []
                 for teacher in teachers:
                     model_output_t = teacher(x)
-                    probs.append(get_prob(model_output_t).unsqueeze(0)) # 1 x class x query x batch
+                    probs.append(get_prob(model_output_t, y, opt.num_support_tr).unsqueeze(0)) # 1 x class x query x batch
                 probs = torch.cat(probs, dim=0) # teacher x class x query x batch
                 teacher_targets = torch.mean(probs, dim=0) # class x query x batch
 
