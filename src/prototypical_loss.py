43ab43c72438136a4a821121769cce02c7fc7125
MattWallingford <mcw244@cornell.edu>
2018-12-04 20:35:45 -0500

    sync

--- src/prototypical_loss.py
+++ src/prototypical_loss.py
@@ -87,16 +87,16 @@ def prototypical_loss(input, target, n_support, teacher_targets=None):
     criterion = nn.CrossEntropyLoss()
     #Added soft labeling in CE and kd loss
     if teacher_targets is None:
-        loss_val = criterion(-dists, target_inds1)
+        log_p_y_reshape = log_p_y.view(n_classes*n_query, -1)
+        one_hot_target = torch.eye(n_classes)[target_inds1]
+        loss_val = KL_loss(log_p_y_reshape, one_hot_target)
+        #loss_val = criterion(-dists, target_inds1)
     else:
         
         one_hot_target = torch.eye(n_classes)[target_inds1]
         log_p_y_reshape = log_p_y.view(n_classes*n_query, -1)
         #loss_val = criterion(-dists, target_inds1) + cross_entropy_soft(-dists, teacher_targets_cpu.view(n_classes*n_query,n_classes))
         alpha = .9
-        print(log_p_y_reshape)
-        print(one_hot_target)
-        print(KL_loss(log_p_y_reshape, one_hot_target), KL_loss(log_p_y_reshape, teacher_targets_cpu))
         loss_val = alpha*KL_loss(log_p_y_reshape, one_hot_target) + (1-alpha)*KL_loss(log_p_y_reshape, teacher_targets_cpu)
     _, y_hat = log_p_y.max(2)
     acc_val = y_hat.eq(target_inds.squeeze()).float().mean()
@@ -158,7 +158,6 @@ def cross_entropy_soft(input, target, size_average=True):
 def KL_loss(x, y):
     output = F.kl_div(x, y, size_average = False)
 
-    print(x.size(0))
     return output/x.size(0)
 
 
